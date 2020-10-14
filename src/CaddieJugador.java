9df218890a14eb04cf814ac15c15ab7f1471dab3
valevanec <vvanegas@unal.edu.co>
2020-10-14 16:07:04 -0500

    correción role

--- src/CaddieJugador.java
+++ src/CaddieJugador.java
@@ -2,7 +2,7 @@ public class CaddieJugador extends Persona {
     public String horario;
 
 
-    public CaddieJugador(String nombre, int edad, int telefono, int documento, String categoria, String contraseña, String horario, String role) {
+    public CaddieJugador(String nombre, int edad, int telefono, int documento, String categoria, String contraseña, String horario, String rol) {
         super(nombre, edad, telefono, documento, categoria, contraseña, rol);
         this.horario = horario;
     }
